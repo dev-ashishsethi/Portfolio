78be0044ef23924a6e336002df237c7ad42860ee
dev-ashishsethi <me.ashishsethi@gmail.com>
2021-12-31 12:17:42 +0530

    feat: skills page complete

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 let reactlogo= document.querySelector("#ReactLogo");
+let reactheading= document.querySelector(".ReactJsBubble__heading");
 
-reactlogo.addEventListener("hover",)
+
+// reactlogo.addEventListener("mouseover", e=>{
+//     reactheading.style.display="none";
+    
+// })
+
+// reactlogo.addEventListener("mouseout", e=>{
+//     reactheading.style.display="inline-block";
+    
+// })
